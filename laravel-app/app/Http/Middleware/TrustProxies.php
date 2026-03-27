d6f5f2370b848621b335e64ab08ad15ba2dc3915
LuisRz1 <langulor1@upao.edu.pe>
2026-03-27 01:04:14 -0500

    Cambios de https v3

--- laravel-app/app/Http/Middleware/TrustProxies.php
+++ laravel-app/app/Http/Middleware/TrustProxies.php
@@ -2,7 +2,17 @@
 
 namespace App\Http\Middleware;
 
-class TrustProxies
-{
+use Illuminate\Http\Request;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 
+class TrustProxies extends Middleware
+{
+    protected $proxies = '*';
+
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB;
 }
